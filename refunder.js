e514303805e24c21392af9d9aa3221a101072dd5
natrix_dev <discordlenko@gmail.com>
2021-10-09 11:26:54 +0200

    Create refunder.js
    Refunder using js

---
+++ refunder.js
@@ -0,0 +1,20 @@
+js.
+
+const natrix = require("natrix-refunder")
+const amazon = require("amazon-natrix")
+
+using amazon !natrix 
+
+console.log("Choose your article (url only)")
+check (if) response.link
+else end(if)
+
+else 
+console.log("refund on-going")
+  refun.natrix
+    amazon.db.refund 
+      article = response.url
+      url.URL 
+    refund()
+    
+  console.log("refund ok!")
